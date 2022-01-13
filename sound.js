13a4b3a74a105bb1388d76805d6702a54fe4cd9f
needdeveloper <87564439+needdeveloper@users.noreply.github.com>
2022-01-13 20:04:29 +0100

    Update sound.js

--- sound.js
+++ sound.js
@@ -2,5 +2,5 @@ function sound(){
 	document.getElementById('sound').play();
   window.setTimeout(function(){
     window.location.href = "https://www.1milliondogs.com/buypixels.html";
-  },500)
+  },600)
 }
